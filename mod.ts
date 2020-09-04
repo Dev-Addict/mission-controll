2353c7d5062a12636ff6df0d3c4ea1b102cae524
WolfMAN <aria.azadi.pour@protonmail.com>
2020-09-05 02:04:54 +0430

    static rotue

--- mod.ts
+++ mod.ts
@@ -1,11 +1,11 @@
-import { Application } from "https://deno.land/x/oak@v6.1.0/mod.ts";
+import { Application, send } from "https://deno.land/x/oak@v6.1.0/mod.ts";
 
 const app = new Application();
 
 app.use(async ({ request: { method, url }, response }, next) => {
   await next();
   const time = response.headers.get("X-Response-Time");
-  console.log(`${method} ${url}: ${time}`);
+  console.log(`${response.status} ${method} ${url}: ${time}`);
 });
 
 app.use(async ({ response }, next) => {
@@ -13,6 +13,21 @@ app.use(async ({ response }, next) => {
   await next();
   const time = Date.now() - start;
   response.headers.set("X-Response-Time", `${time}ms`);
+});
+
+app.use(async (ctx) => {
+  const filePath = ctx.request.url.pathname;
+
+  const fileWhitelist = [
+    "/index.html",
+    "/javascript/script.js",
+    "/stylesheet/style.css",
+    "/images/favicon.png",
+  ];
+
+  await send(ctx, filePath, {
+    root: `${Deno.cwd()}/public`,
+  });
 });
 
 app.use(({ response }) => {
